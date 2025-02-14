edebd37b9b5efa9ca18931759fe289474dd1af66
yusufarslanalp <ayusufa42@gmail.com>
2025-02-14 06:31:43 +0300

    add three transportation route algorithm. algorithm not tested.

--- src/main/java/com/example/repository/TransportationRepository.java
+++ src/main/java/com/example/repository/TransportationRepository.java
@@ -4,10 +4,18 @@ package com.example.repository;
 import com.example.model.Location;
 import com.example.model.Transportation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TransportationRepository extends JpaRepository<Transportation, Long> {
     List<Transportation> findByFrom(Location from);
 
+    List<Transportation> findByTo(Location from);
+
+    //@Query("SELECT t FROM Transportation t WHERE t.from IN :fromList AND t.to IN :toList")
+    //List<Transportation> findByFromInAndToIn(@Param("fromList") List<Location> fromList, @Param("toList") List<Location> toList);
+
+    List<Transportation> findByFromAndTo(Location from, Location to);
 }
--- src/main/java/com/example/service/RouteService.java
+++ src/main/java/com/example/service/RouteService.java
@@ -2,13 +2,12 @@ package com.example.service;
 
 import com.example.model.Location;
 import com.example.model.Transportation;
-import com.example.model.TransportationType;
-import com.example.repository.LocationRepository;
 import com.example.repository.TransportationRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -16,31 +15,29 @@ import java.util.List;
 public class RouteService {
     private final TransportationRepository transportationRepository;
 
-
     public List<List<Transportation>> getRoutes(Location from, Location to){
-        //transportationRepository.findByFrom(from);
-        return Arrays.asList(
-          Arrays.asList(
-                  Transportation.builder()
-                          .id(1L)
-                          .from(Location.builder().name("Şirinevler").build())
-                          .to(Location.builder().name("Istanbul Hava Alanı").build())
-                          .type(TransportationType.BUS)
-                          .build(),
-                  Transportation.builder()
-                          .id(2L)
-                          .from(Location.builder().name("Istanbul Hava Alanı").build())
-                          .to(Location.builder().name("Esenboğa Hava Alanı").build())
-                          .type(TransportationType.FLIGHT)
-                          .build(),
-                  Transportation.builder()
-                          .id(3L)
-                          .from(Location.builder().name("Esenboğa Hava Alanı").build())
-                          .to(Location.builder().name("Kızılay Metdanı").build())
-                          .type(TransportationType.BUS)
-                          .build()
-          )
-        );
+        List<List<Transportation>> routes = Collections.emptyList();
+
+        List<Transportation> firstTransports = transportationRepository.findByFrom(from);
+        List<Transportation> lastTransports = transportationRepository.findByTo(to);
+
+        for(Transportation firsTransport : firstTransports){
+            for(Transportation lastTransport : lastTransports){
+                List<Transportation> connector = transportationRepository
+                        .findByFromAndTo(firsTransport.getTo(), lastTransport.getFrom());
+                if(connector.size() > 0){
+                    routes.add(
+                            Arrays.asList(
+                                    firsTransport,
+                                    connector.get(0),
+                                    lastTransport
+                            )
+                    );
+                }
+            }
+        }
+
+        return null;
     }
 
 }
