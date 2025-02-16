06c0e328219b42006f0b08f936078941727dc86e
yusufarslanalp <ayusufa42@gmail.com>
2025-02-16 20:59:38 +0300

    feat: add locationController

---
+++ src/main/java/com/example/controller/LocationController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.model.Location;
+import com.example.service.LocationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping( "location" )
+@AllArgsConstructor
+public class LocationController {
+    private final LocationService locationService;
+
+    @GetMapping
+    public List<Location> getAllLocations(){
+        return locationService.getLocations();
+    }
+
+
+    @PostMapping
+    public void createLocation(@RequestBody Location location){
+        locationService.saveLocation(location);
+    }
+
+    @DeleteMapping("/{locationId}")
+    public void deleteLocation(@PathVariable Long locationId){
+        locationService.deleteLocation(locationId);
+    }
+}
---
+++ src/main/java/com/example/service/LocationService.java
@@ -0,0 +1,29 @@
+package com.example.service;
+
+
+import com.example.model.Location;
+import com.example.repository.LocationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+
+
+@Component
+@AllArgsConstructor
+public class LocationService {
+    private final LocationRepository locationRepository;
+
+    public List<Location> getLocations(){
+        return locationRepository.findAll();
+    }
+
+    public void saveLocation(Location location){
+        locationRepository.save(location);
+    }
+
+    public void deleteLocation(Long locationId){
+        locationRepository.deleteById(locationId);
+    }
+}
